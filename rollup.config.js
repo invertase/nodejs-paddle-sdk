12dca0e3f3a54410390d1e2d5c4780bcf0f45add
Elliot Hesp <elliot.hesp@gmail.com>
2023-03-07 10:01:04 +0000

    chore: 0.3.3

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: pkg.main,
+      file: 'dist/node-paddle-sdk.cjs.js',
       format: 'cjs',
     },
     {
-      file: pkg.module,
+      file: 'dist/node-paddle-sdk.es.js',
       format: 'es',
     },
   ],
